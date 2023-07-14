0b0a3c687ce58203407a503ca44100ee64eaf31b
unknown <isratjahan.emu1998@gmail.com>
2023-07-14 20:34:37 +0600

    routing setup

---
+++ src/routes/routes.tsx
@@ -0,0 +1,29 @@
+import {createBrowserRouter} from "react-router-dom";
+import NotFound from "../pages/NotFound";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import App from "../App";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default routes;

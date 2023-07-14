df6ea96624a4e943d1905a72cd1084343b93a50c
unknown <isratjahan.emu1998@gmail.com>
2023-07-14 21:14:41 +0600

    added color palatte

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import {useState} from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <p className="bg-accent">assdfdg</p>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textColor: "var(--text-color)",
+        fill: "var(--background-color)",
+        second: "var(--secondary-color)",
+        primary: "var(--primary-color)",
+        accent: "var(--accent-color)",
+      },
+    },
   },
   plugins: [],
 };
